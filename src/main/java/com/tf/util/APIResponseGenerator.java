eba73b12a97beec8a6bd45620c592b556940e05f
BeaverCoder <parth.mahajan95@gmail.com>
2018-08-24 16:03:54 +0530

    Add CORS integration

--- src/main/java/com/tf/util/APIResponseGenerator.java
+++ src/main/java/com/tf/util/APIResponseGenerator.java
@@ -15,6 +15,8 @@ public class APIResponseGenerator {
 		Map<String, String> headers = new HashMap<>();
 		headers.put("X-Powered-By", "AWS Lambda & Serverless");
 		headers.put("Content-Type", "application/json");
+		headers.put("Access-Control-Allow-Origin", "*");
+		headers.put("Access-Control-Allow-Headers", "X-Requested-With");
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(responseBody)
